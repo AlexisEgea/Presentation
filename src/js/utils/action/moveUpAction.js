801b1d810501f5f0dda6ddd185812bc16a8a0bc5
AlexisEgea <alexisegea@outlook.com>
2025-06-09 20:33:27 +0200

    Updating comment and Refactoring

--- src/js/utils/action/moveUpAction.js
+++ src/js/utils/action/moveUpAction.js
@@ -1,28 +1,16 @@
-import { displayContent } from '../typeAnimation.js';
+import { displayContentWithTypingAnimation } from '../typeAnimation.js';
 
 export function addMoveUpAction(section) {
     const elements = ['title', 'subtitle'].map(type => 
         document.querySelector('.' + section + '-' + type)
     );
+        const sectionElement = document.querySelector('.' + section);
+
     
     const moveUpListener = (event) => {
         // Prevent click event from propagating to parent
         event.stopPropagation();
-        
-        // TODO: Upgrade this section for content, it doesn't fix the position of content
-        const sectionElement = document.querySelector('.' + section);
-        // If section is already 'ready', place elements directly at the top
-        if (sectionElement && sectionElement.classList.contains('ready')) {
-            elements.forEach(element => {
-                if (element) {
-                    element.style.transform = 'translateY(-35vh)';
-                }
-            });
-            let content = document.querySelector(".introduction-content");
-            content.style.position = "absolute";
-            displayContent(section);
-            return;
-        }
+    
         
         // Add move-up class to all elements
         elements.forEach(element => {
@@ -44,15 +32,14 @@ export function addMoveUpAction(section) {
             if (element) {
                 element.addEventListener('animationend', () => {
                     clickElement.innerText = "Click to skip typing animation";
-                    let content = document.querySelector(".introduction-content");
+                    let content = sectionElement.querySelector(".introduction-content");
                     content.style.position = "absolute";
-                    displayContent(section);
+                    displayContentWithTypingAnimation(section);
                 }, { once: true }); // Remove listener after first trigger
             }
         });
     };
     // Add click listener
-    const sectionElement = document.querySelector('.' + section);
     if (sectionElement) {
         sectionElement.addEventListener('click', moveUpListener);
     }
