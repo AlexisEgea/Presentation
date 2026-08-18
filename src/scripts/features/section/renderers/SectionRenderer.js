71a8d73a81fabcde46d7679f678cc34f760e9851
AlexisEgea <alexisegea@oulook.com>
2026-08-19 00:12:51 +0200

    Adding section renderer

---
+++ src/scripts/features/section/renderers/SectionRenderer.js
@@ -0,0 +1,25 @@
+// Base class for section page renderers.
+export class Section {
+    constructor(sectionName, container) {
+        this.sectionName = sectionName;
+        this.container = container;
+    }
+
+    clear() {
+        this.container.innerHTML = '';
+    }
+
+    createElement(tagName, className, textContent) {
+        const element = document.createElement(tagName);
+        if (className) {
+            element.className = className;
+        }
+        element.textContent = textContent;
+        return element;
+    }
+
+    // Must be implemented by child renderers.
+    async render() {
+        throw new Error('render() must be implemented by a Section subclass');
+    }
+}
